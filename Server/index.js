d31fd74a00ca83bb5e69f5a933c0c1e35423be8d
Krishmadhotra <ckrishmadhotra@gmail.com>
2024-06-16 16:59:17 +0530

    crreated a middleware and functions to check errors

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-
+import userRoutes from './routes/user.route.js';
+import authRoutes from "./routes/auth.route.js"
 
 const app=express();
 dotenv.config();
-
+app.use(express.json())
 mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log("MongoDB is connected")
@@ -17,3 +18,17 @@ mongoose.connect(process.env.MONGO)
 app.listen(3000,()=>{
     console.log("app is running on port 300 !")
 })
+
+app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes)
+
+app.use((err,req,res,next)=>{
+    const statuscode=err.statuscode || 500;
+    const errMessage=err.message ||'Internal Server Error';
+    res.status(statuscode).json({
+        success:false,
+        statuscode,
+        errMessage
+    })
+
+});
